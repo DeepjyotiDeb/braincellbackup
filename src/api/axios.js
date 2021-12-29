b55f48ec2c2d88e6da07627708135a250ad5e276
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2021-12-29 16:05:55 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,7 +6,8 @@ const service = axios.create({
 
   // service.defaults.baseURL = 'http://localhost:8000/';
 //   service.defaults.baseURL ='https://zwc74bqvu4.execute-api.ap-south-1.amazonaws.com/dev1/'
-service.defaults.baseURL = 'https://8ni54ztj2i.execute-api.ap-south-1.amazonaws.com/dev3'
+// service.defaults.baseURL = 'https://8ni54ztj2i.execute-api.ap-south-1.amazonaws.com/dev3'
+  service.defaults.baseURL = 'https://2k17ah.deta.dev/';
   service.defaults.headers.post['Content-Type'] = 'application/json';
   
   // request interceptor
