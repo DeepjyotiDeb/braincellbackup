be0deb6d194850577059c06916656484513ac852
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2021-12-30 20:11:54 +0530

    Update Post.js

--- src/home/blog-container/post/Post.js
+++ src/home/blog-container/post/Post.js
@@ -6,19 +6,16 @@ export default function Post(props) {
     const postData = props.postData;
 
     return (
-        <Box m={3}>
+        <Box m={3}><Link to={`/view-post/${postData.id}`} style={{ textDecoration: 'none' }}>
             <Card>
-                <CardHeader title={<Link to={`/view-post/${postData.id}`} 
-                            style = {{ color: "inherit", underline : "never"}}>
-                                {postData.id} {postData.title}
-                                    </Link>}>
+                <CardHeader
+                title = {postData.title}>
                 </CardHeader>
                 <CardContent>
                     <Typography variant="h5">{postData.summary}</Typography>
-                    {/* <Typography variant="body">{postData.body}</Typography> */}
                     <div><Typography variant="h10" m={4}>Created on {postData.created_on}</Typography></div>
                 </CardContent>
-            </Card>
+            </Card></Link>
         </Box>
     )
 }
