df6c8254619a6b035432cca1bfea15e97bf040b0
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-01-26 11:52:46 +0530

    Update SinglePost.js

--- src/home/blog-container/singlepost/SinglePost.js
+++ src/home/blog-container/singlepost/SinglePost.js
@@ -3,11 +3,13 @@ import React, {useEffect, useState} from 'react'
 import axios from "../../../api/axios"
 import { useNavigate, useParams } from 'react-router-dom';
 import { Link, } from "react-router-dom";
+import ReactLoading from "react-loading"
 
 export default function SinglePost() {
     const navigate = useNavigate()
     const { id } = useParams(); // Reads the URL on the URL Bar and gets whatever is after ":"
     const [post, setPost] = useState({});
+    const [done, setDone] = useState(undefined)
 
     const handleSubmit= () => {
         axios.delete(`/delete-post/${id}`, 
@@ -20,40 +22,61 @@ export default function SinglePost() {
 
     // mount
     useEffect(() => {
+        setTimeout(() => {
         axios.get(`/get-post/${id}`)
         .then(function (response) {
             // handle success
             setPost(response.data.post)
+            setDone(true)
         })
         .catch(function (error) {
             // handle error
             console.log(error);
         });
+        },500)    
     }, []);
 
     const SuperButton = () => {
-        if(post.user_id == localStorage.getItem('user_id')){return(<>
+        if(post.user_id == localStorage.getItem('user_id')){
+            return(<>
             <Button variant = "contained" 
                     color ="success"><Link to={`/update-post/${post.id}`} 
                     style = {{textDecoration: "none", color: "inherit"}}>edit post </Link></Button>
                 <Button variant = "contained"
-                    color = "error" onClick = {handleSubmit}>delete post</Button></>)
+                    color = "error" onClick = {handleSubmit}>delete post</Button>
+                   </>)
         }else{
+           
             // return(console.log("empty"))
         }
     }
     
-    return (
-        <Container maxWidth="lg">
+    return (<>{!done?(
+        <div
+            style={{
+                position: 'absolute', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)' //centering
+            }}
+        >
+        <ReactLoading
+            type={"balls"}
+            color={"#7D3C98"}
+            height={50}
+            width={50}/>
+        </div>
+        ) : (
+            <Container maxWidth="lg">
             <Card>
                 <CardHeader title={`${post.title}`}></CardHeader>
                 <CardContent><Typography align = "left">
                     <Typography variant="h5">{post.summary}</Typography>                   
                     <div dangerouslySetInnerHTML={{ __html: post.body}} />
-                    <Typography variant="h10" m={4}>Created on {post.created_on}</Typography></Typography>
+                    <div><Typography variant="h10" m={4}>Created on {post.created_on}</Typography></div></Typography>
                 </CardContent>
             </Card>
-                {SuperButton}
+                {SuperButton()}
         </Container>
+        )}</>
+        
     )
 }
