80c845482314210ca064968c1b0381e7f8b94d89
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-01-26 19:44:55 +0530

    Update BlogContainer.js

--- src/home/blog-container/BlogContainer.js
+++ src/home/blog-container/BlogContainer.js
@@ -2,34 +2,51 @@ import { Container } from '@mui/material'
 import React, {useEffect, useState} from 'react'
 import Post from './post/Post'
 import axios from "../../api/axios";
+import ReactLoading from "react-loading"
 
 export default function BlogContainer() {
 
     const [posts, setPosts] = useState([]);
-
+    const [done, setDone] = useState(undefined)
     // mount
     useEffect(() => {
-        console.log("This got called")
+        setTimeout(() => {
         axios.get('/posts')
         .then(function (response) {
             // handle success
-            console.log('here is my data', response.data);
+            // console.log('here is my data', response.data);
             setPosts(response.data.posts)
+            setDone(true)
         })
         .catch(function (error) {
             // handle error
-            console.log(error);
-            // <h1>nothing</h1>
+            console.log(error);            
         });
+        }, 500)        
     }, []);
 
     const postComponents = posts.map(item => {
-        return <Post key={item.id} postData={item}></Post>
+        return <Post key={item.id} postData={item}></Post> //passing item as props to <Post>
     })
 
     return (
-        <Container maxWidth="lg">
+        <>{!done? (
+            <div
+            style={{
+                position: 'absolute', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)' //centering
+            }}
+        >
+        <ReactLoading
+            type={"balls"}
+            color={"#7D3C98"}
+            height={50}
+            width={50}/>
+        </div>  
+        ) : (
+            <Container maxWidth="lg">
             {postComponents}
         </Container>
-    )
+        )}        
+        </>)
 }
