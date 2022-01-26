6de3484d15f6f4f73b8285a374e19c848c5366a4
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-01-26 13:07:45 +0530

    Update UpdatePost.js

--- src/home/updatepost/UpdatePost.js
+++ src/home/updatepost/UpdatePost.js
@@ -5,6 +5,7 @@ import { useNavigate,useParams } from "react-router-dom"
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import "../makepost/styles.css"
+import moment from 'moment'
 
 export default function UpdatePost() {
     let navigate = useNavigate();
@@ -44,21 +45,21 @@ export default function UpdatePost() {
         .then(()=> navigate('/'))    
       }
     // mount    
-    
+  
     return (
         <Container maxWidth="lg">
             <Card><Typography align = "left" m={2}>
                 <CardHeader title={`Currently Viewing:- ${post.title}`}></CardHeader>
                     <Typography variant="h5">{post.summary}</Typography>
                     <div dangerouslySetInnerHTML={{ __html: post.body}} />
-                    <Typography variant="h10" m={4}>Created on {post.created_on}</Typography></Typography>
+                    <Typography variant="caption" m={4}>
+                    Created on {moment(post.created_on).format('MMMM Do YYYY, h:mm a')}
+                      </Typography></Typography>
             </Card>
             <Box component="form" noValidate autoComplete="off">
             <div>
       <div>    
     <TextField
-      // defaultValue={post.title}
-      // inputRef={(input) => post.title = input }
       onChange={handleChange}
       id="outlined-textarea"
       label="title"
@@ -81,11 +82,10 @@ export default function UpdatePost() {
         <div className="editor">
         <div className="cke">
         <CKEditor 
-        editor={ ClassicEditor }        
-        onChange={ ( event, editor ) => {
+          editor={ ClassicEditor }        
+          onChange={ ( event, editor ) => {
             const data = editor.getData();           
-            setBody(data)
-            // setValues(values.body.data)          
+            setBody(data)       
           } }        
     /></div>
     </div><div>  
